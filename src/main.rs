c39f24a93c029acc2c1a654dfdf17e47bb279d41
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 20:20:54 +0300

    python script called

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use std::process::Command;
 
-fn main() -> Result<()> {
-    Ok(())
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+  // Run the "add_route/main.py" Python script and capture its output
+  let output = Command::new("python3")
+      .arg("add_route/main.py") // Argument for the Python script
+      .output()
+      .expect("Failed to run Python script"); // Error message
+
+
+  Ok(())
 }
