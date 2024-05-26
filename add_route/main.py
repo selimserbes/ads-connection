4d084659dd214339dc317cc9b7ab1aff351a158b
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 20:16:28 +0300

    route addition is completed

--- add_route/main.py
+++ add_route/main.py
@@ -14,5 +14,22 @@ plc_password = "1"
 # Define the name for the route to be added
 route_name = "plc"
 
+# Open communication with the ADS server
+pyads.open_port()
 
+# Set the local AMS address for the PC
+pyads.set_local_address(pc_ams)
 
+# Add a route to connect to the PLC
+pyads.add_route_to_plc(
+    plc_ams,  # AMS address of the PLC
+    pc_ip,  # IP address of the PC
+    plc_ip,  # IP address of the PLC
+    plc_username,  # Username for PLC access (if required)
+    plc_password,  # Password for PLC access (if required)
+    route_name,  # Name for the route
+    pc_ams,  # AMS address of the PC (used for return communication)
+)
+
+# Close communication with the ADS server
+pyads.close_port()
