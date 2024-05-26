fb00339c5b9ee1579ed747eb1f0294d94d0956d9
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 20:12:02 +0300

    plc authentication informations added

--- add_route/main.py
+++ add_route/main.py
@@ -7,6 +7,9 @@ pc_ams = "192.168.1.5.1.1"  # AMS address of the PC
 plc_ip = "192.168.2.3"
 plc_ams = "192.168.2.3.1.1"  # AMS address of the PLC
 
+# Define credentials for accessing the PLC (if required)
+plc_username = "Administrator"
+plc_password = "1"
 
 
 
