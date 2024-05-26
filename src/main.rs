722055b0dea10dbab4b25d55c941d1645c465928
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 20:29:15 +0300

    write a value to plc

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let value: u32 = handle.read_value()?;
     println!("GVL.VARIABLE old value is {}", value);
 
+    // Write a value to the PLC (left as a comment for now)
+    // let value_to_write: u32 = 1;
+    // handle.write_value(&value_to_write)?;
+
     println!("Python script executed successfully: {:?}", output);
   } else {
     // Print an error message if the Python script failed
